beb19c8e8de2f8aff254cf6bced89bee0f692719
Arinjoy <arinjoy.biswas@hotmail.com>
2023-11-24 11:32:26 +1100

    create a skeleton app project
     - add assets/icons
    - make a simple tab-view based swiftUi app

---
+++ MusicHub/MusicHub/MainView.swift
@@ -0,0 +1,49 @@
+//
+//  Created by Arinjoy Biswas on 24/11/2023.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        TabView {
+            roomsView
+                .tabItem {
+                    Image(.tabbarIconBlockRooms)
+                    Text("Rooms")
+                }
+
+            nowPlayingView
+                .tabItem {
+                    Image(.tabbarIconNowPlaying)
+                    Text("Now Playing")
+                }
+
+            settingsView
+                .tabItem {
+                    Image(.tabbarIconSettings)
+                    Text("Settings")
+                }
+        }
+        .tint(.teal)
+    }
+
+    @ViewBuilder
+    private var roomsView: some View {
+        Text("List of rooms/devices")
+    }
+
+    @ViewBuilder
+    private var nowPlayingView: some View {
+        Text("Settings")
+    }
+
+    @ViewBuilder
+    private var settingsView: some View {
+        Text("Settings")
+    }
+}
+
+#Preview {
+    MainView()
+}
---
+++ MusicHub/MusicHub/MusicHubApp.swift
@@ -0,0 +1,14 @@
+//
+//  Created by Arinjoy Biswas on 24/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct MusicHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
